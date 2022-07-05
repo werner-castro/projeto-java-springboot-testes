49ab9ebdc24e8e50357193ffcec0063bc494f37a
werner-castro <carlos.werner@pathfind.com.br>
2022-07-05 09:45:58 -0300

    add UserRepository

---
+++ src/main/java/com/example/api/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.api.repositories;
+
+import com.example.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
