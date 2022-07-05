244c03c2e6bd3dd31b9c96b4195c1a54da30e2bf
werner-castro <carlos.werner@pathfind.com.br>
2022-07-05 09:45:47 -0300

    add UserController

---
+++ src/main/java/com/example/api/resources/UserController.java
@@ -0,0 +1,24 @@
+package com.example.api.resources;
+
+import com.example.api.domain.User;
+import com.example.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @Autowired
+    UserService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(service.findById(id));
+    }
+
+}
