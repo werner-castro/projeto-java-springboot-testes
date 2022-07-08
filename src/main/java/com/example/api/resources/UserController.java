4980c27d47969b7989a2ba80b5a6423f3cd9e45a
werner-castro <carlos.werner@pathfind.com.br>
2022-07-08 08:26:24 -0300

    adicionando uma constante para o id do user

--- src/main/java/com/example/api/resources/UserController.java
+++ src/main/java/com/example/api/resources/UserController.java
@@ -1,6 +1,5 @@
 package com.example.api.resources;
 
-import com.example.api.domain.User;
 import com.example.api.domain.dto.UserDTO;
 import com.example.api.services.UserService;
 import org.modelmapper.ModelMapper;
@@ -17,6 +16,7 @@ import java.util.stream.Collectors;
 @RequestMapping(value = "/user")
 public class UserController {
 
+    public static final String ID = "/{id}";
     @Autowired
     private ModelMapper mapper;
     @Autowired
@@ -24,7 +24,7 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserDTO> create(@RequestBody UserDTO obj){
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path(ID).buildAndExpand(
                 service.create(obj).getId()
         ).toUri();
         return ResponseEntity.created(uri).build();
@@ -37,17 +37,22 @@ public class UserController {
         );
     }
 
-    @PutMapping(value = "/{id}")
+    @PutMapping(value = ID)
     public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj){
         obj.setId(id);
         return ResponseEntity.ok().body(mapper.map(service.update(obj), UserDTO.class));
     }
 
-    @GetMapping(value = "/{id}")
+    @DeleteMapping(value = ID)
+    public ResponseEntity<UserDTO> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = ID)
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
         return ResponseEntity.ok().body(
                 mapper.map(service.findById(id), UserDTO.class)
         );
     }
-
 }
