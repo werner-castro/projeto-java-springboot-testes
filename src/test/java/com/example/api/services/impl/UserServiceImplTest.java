157f635385516a6c61dfd78305783cea0268d844
werner-castro <carlos.werner@pathfind.com.br>
2022-07-10 03:33:16 -0300

    teste na classe UserServiceImplTest no metodo findAll

--- src/test/java/com/example/api/services/impl/UserServiceImplTest.java
+++ src/test/java/com/example/api/services/impl/UserServiceImplTest.java
@@ -2,6 +2,7 @@ package com.example.api.services.impl;
 
 import com.example.api.domain.User;
 import com.example.api.domain.dto.UserDTO;
+import com.example.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.example.api.repositories.UserRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -23,6 +25,8 @@ class UserServiceImplTest {
     public static final String NAME     = "werner";
     public static final String EMAIL    = "werner@gmail.com";
     public static final String PASSWORD = "admin";
+    public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+    public static final int INDEX = 0;
 
     // adicionando as dependências da classe UserService que vai ser testada
 
@@ -89,7 +93,47 @@ class UserServiceImplTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindByIdThenReturnAnObjectNotFoundException(){
+        // quando o repository.findbyId for chamado, passamos um Id do tipo inteiro,
+        // então retornamos uma excessão informando objeto não encontrado.
+        Mockito.when(repository.findById(Mockito.anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
+
+        try {
+            service.findById(ID);
+        } catch (Exception ex) {
+            Assertions.assertEquals(ObjectNotFoundException.class, ex.getClass());
+            Assertions.assertEquals(OBJETO_NAO_ENCONTRADO, ex.getMessage());
+        }
+    }
+
+    @Test
+    void whenFindAllThenReturnAnListOfUsers() {
+
+        // gerando o mock
+        // quando o repository.findAll for chamado, devemos retornar uma lista de users
+        Mockito.when(repository.findAll()).thenReturn(List.of(user));
+
+        //acessando o mock
+        List<User> response = service.findAll();
+
+        // teste de verificação de valores nulos
+        Assertions.assertNotNull(response);
+
+        // teste de verificação do tamanho da lista de users:
+        // nesse caso, tamanho da lista é igual á um
+        Assertions.assertEquals(1, response.size());
+
+        // teste: verificar se o primento elemento da lista de users seja do tipo User
+        Assertions.assertEquals(User.class, response.get(INDEX).getClass());
+
+        // teste: verificar se o ID passado na requisição seja igual ao ID do primeiro elemento da lista retornada
+        Assertions.assertEquals(ID, response.get(INDEX).getId());
+
+        // teste: verificar se o NAME passado na requisição seja igual ao NAME do primeiro elemento da lista retornada
+        Assertions.assertEquals(NAME, response.get(INDEX).getName());
+
+        // teste: verificar se o EMAIL passado na requisição seja igual ao EMAIL do primeiro elemento da lista retornada
+        Assertions.assertEquals(EMAIL, response.get(INDEX).getEmail());
     }
 
     @Test
