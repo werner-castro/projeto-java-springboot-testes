45a79d81481d86974a538302aa9baba8590a2651
werner-castro <carlos.werner@pathfind.com.br>
2022-07-19 12:15:47 -0300

    adicao da validacao notNull no metodo update

--- src/main/java/com/example/api/resources/UserController.java
+++ src/main/java/com/example/api/resources/UserController.java
@@ -2,6 +2,7 @@ package com.example.api.resources;
 
 import com.example.api.domain.dto.UserDTO;
 import com.example.api.services.UserService;
+import org.jetbrains.annotations.NotNull;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -39,7 +40,7 @@ public class UserController {
     }
 
     @PutMapping(value = ID)
-    public ResponseEntity<UserDTO> update(@Valid @PathVariable Integer id, @RequestBody UserDTO obj){
+    public ResponseEntity<UserDTO> update(@Valid @PathVariable Integer id, @RequestBody @NotNull UserDTO obj){
         obj.setId(id);
         return ResponseEntity.ok().body(
                 mapper.map(service.update(obj), UserDTO.class)
